861fdc8ccc0076ff5de5261bc02f78e5003ccbf6
Marek Marecki <marekjm@ozro.pw>
2017-09-15 23:28:55 +0200

    Annotated version of sum_if_true

--- part_i/chapter_01/sum_if_true.ml
+++ part_i/chapter_01/sum_if_true.ml
@@ -8,10 +8,15 @@
 let sum_if_true test first second =
     (if test first then first else 0) + (if test second then second else 0)
 
+let sum_if_true_annot (test : int -> bool) (x : int) (y : int) =
+    (if test x then x else 0) + (if test y then y else 0)
+
 let even x =
     x mod 2 = 0
 
 
 let () =
     print_endline (string_of_int (sum_if_true even 3 4));
-    print_endline (string_of_int (sum_if_true even 2 4))
+    print_endline (string_of_int (sum_if_true even 2 4));
+    print_endline (string_of_int (sum_if_true_annot even 3 4));
+    print_endline (string_of_int (sum_if_true_annot even 2 4))
