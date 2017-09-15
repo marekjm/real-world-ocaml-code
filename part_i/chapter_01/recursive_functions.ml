399bd0d1987e171c2e6bccd0c56fe5428dd824c5
Marek Marecki <marekjm@ozro.pw>
2017-09-16 00:19:47 +0200

    Tail-recursive sum

--- part_i/chapter_01/recursive_functions.ml
+++ part_i/chapter_01/recursive_functions.ml
@@ -5,6 +5,14 @@ let rec sum l =
     | [] -> 0   (* base case *)
     | hd :: tl -> hd + sum tl   (* inductive case *)
 
+let rec sum_tail_impl l acc =
+    match l with
+    | [] -> acc     (* base case *)
+    | hd :: tl -> sum_tail_impl tl (acc + hd)   (* inductive case *)
+
+let sum_tail l = sum_tail_impl l 0
+
 
 let () =
-    print_endline (string_of_int (sum [1; 2; 3]))
+    print_endline (string_of_int (sum [1; 2; 3]));
+    print_endline (string_of_int (sum_tail [1; 2; 3]))
