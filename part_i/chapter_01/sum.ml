f8afa577f42e247d6b173d825a448ef1674a8b5a
Marek Marecki <marekjm@ozro.pw>
2017-09-22 19:24:07 +0200

    Looks good but enters infinite loop after first [Enter]

--- part_i/chapter_01/sum.ml
+++ part_i/chapter_01/sum.ml
@@ -5,20 +5,29 @@
  * The below code will not work if you have glibc 2.26.
  * It appears that Core is broken with this version of glibc.
  *)
-let float_from_string = Core.Float.of_string
 (*
+let float_from_string = Core.Float.of_string
 let get_line = Stdio.In_channel.input_line Stdio.In_channel.stdin
+let rec read_and_accumulate accum =
+    (*
+     * The book uses unqualified In_channel (probably because In_channel
+     * was part of Core) but now it must be used through Stdio.
+     * You could use `open Stdio`, but I chose not to.
+     *)
+    let line = get_line in
+    match line with
+    | Some x -> read_and_accumulate (accum +. float_from_string x)
+    | None -> accum
 *)
 
 (*
  * Use this if you have glibc 2.26.
  *)
-(*
-let float_from_string s option =
-    match s with
-    | Some x = float_of_string x
-    | None -> 0.0
-*)
+let float_from_string s =
+    try
+        let f = float_of_string s in Some f
+    with
+    | Failure _ -> None
 
 let get_line =
     try
@@ -34,7 +43,9 @@ let rec read_and_accumulate accum =
      *)
     let line = get_line in
     match line with
-    | Some x -> read_and_accumulate (accum +. float_from_string x)
+    | Some x -> (print_endline "well, hello there"; match float_from_string x with
+                | Some f -> read_and_accumulate (accum +. f)
+                | None -> accum)
     | None -> accum
 
 let () =
