4eec98a44c5c8dd8105e58998b6d8eba8b6b4992
Marek Marecki <marekjm@ozro.pw>
2017-09-22 19:33:46 +0200

    Variables example

---
+++ part_i/chapter_02/variables.ml
@@ -0,0 +1,14 @@
+let languages = "OCaml,Python,C++,C"
+
+let dashed_languages =
+    let language_list = String.split languages ~on:',' in
+    String.concat ~sep:"-" language_list
+
+let area_of_ring inner_radius outer_radius =
+    let pi = acos (-1.) in
+    let area_of_circle r = pi *. r *. r in
+    area_of_circle outer_radius -. area_of_circle inner_radius
+
+let () =
+    print_endline dashed_languages;
+    print_endline (string_of_float (area_of_ring 1. 3))
