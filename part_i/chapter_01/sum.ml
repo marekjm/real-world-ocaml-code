1818402b025dfe1199410c9f55ef1607b0d02f40
Marek Marecki <marekjm@ozro.pw>
2017-09-22 19:08:23 +0200

    OK, code still compiles

--- part_i/chapter_01/sum.ml
+++ part_i/chapter_01/sum.ml
@@ -1,16 +1,19 @@
 (* The book uses `open Core.Std` but it is deprecated. *)
 (* open Core *)
 
+let float_from_string = Core.Float.of_string
+let get_line = Stdio.In_channel.input_line Stdio.In_channel.stdin
+
 let rec read_and_accumulate accum =
     (*
      * The book uses unqualified In_channel (probably because In_channel
      * was part of Core) but now it must be used through Stdio.
      * You could use `open Stdio`, but I chose not to.
      *)
-    let line = Stdio.In_channel.input_line Stdio.In_channel.stdin in
+    let line = get_line in
     match line with
     | None -> accum
-    | Some x -> read_and_accumulate (accum +. Core.Float.of_string x)
+    | Some x -> read_and_accumulate (accum +. float_from_string x)
 
 (*
 let get_line () =
