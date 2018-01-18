8676e4f6e1718f5ca39dd149ffea01275ded124a
Marek Marecki <marekjm@ozro.pw>
2018-01-18 20:12:01 +0100

    Use Map instead of a list

--- part_i/chapter_04/single_file_programs/counter.ml
+++ part_i/chapter_04/single_file_programs/counter.ml
@@ -1,26 +1,15 @@
 open Core
 
-(*
- * Example copied verbatim from the book does not work.
- * Both List.Assoc.find and List.assoc.add need an argument 'equal' of
- * type ('a -> 'a -> bool).
- *
- * So here it is.
- *)
-let comparator a b = a = b
+type t = int String.Map.t
 
-type t = (string * int) list
+let empty = String.Map.empty
 
-let empty = []
-
-let to_list x = x
+let to_list t = Map.to_alist t
 
 let touch t s =
     let count =
-        (* Note the use of comparator. Original example did not have it. *)
-        match List.Assoc.find ~equal:comparator t s with
+        match Map.find t s with
         | None -> 0
         | Some x -> x
     in
-    (* Note the use of comparator. Original example did not have it. *)
-    List.Assoc.add ~equal:comparator t s (count + 1)
+    Map.add t ~key:s ~data:(count + 1)
