b07abb11a02708302faf3527f1451c9ee7d6e936
Marek Marecki <marekjm@ozro.pw>
2018-01-21 12:35:41 +0100

    Made a module for the host info

--- part_i/chapter_05/records/example.ml
+++ part_i/chapter_05/records/example.ml
@@ -1,25 +1,29 @@
 open Core
 open Core_extended
 
-type host_info = {
-    hostname    : string;
-    os_name     : string;
-    cpu_arch    : string;
-    timestamp   : Time.t;
-}
+module Hostinfo : sig
+    type t
+    val to_string : t -> string
+    val make : unit -> t
+end = struct
+    type t = {
+        hostname    : string;
+        os_name     : string;
+        cpu_arch    : string;
+        timestamp   : Time.t;
+    }
 
-let my_host =
-    let sh = Shell.sh_one_exn in {
+    let make () = let sh = Shell.sh_one_exn in {
         hostname = sh "hostname";
         os_name = sh "uname -s";
-        cpu_arch = sh "uname -m";   (* The book uses `uname -p' but it did not work
-                                     * on my machine (returned "unknown").
-                                     * The `-m' switch works better.
-                                     *)
+        cpu_arch = sh "uname -m";
         timestamp = Time.now ();
-}
+    }
 
-let to_string h =
-    h.hostname ^ " " ^ h.os_name ^ " " ^ h.cpu_arch
+    let to_string { hostname = h; os_name = os; cpu_arch = c; timestamp = ts; } =
+        let timezone = Lazy.force Time.Zone.local in
+        let ts = Time.to_sec_string ts ~zone:timezone in
+        sprintf "%s (%s / %s, on %s)" h os c ts
+end
 
-let () = print_endline (to_string my_host)
+let () = print_endline (Hostinfo.to_string (Hostinfo.make ()))
